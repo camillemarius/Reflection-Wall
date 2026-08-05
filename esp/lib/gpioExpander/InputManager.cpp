fc8dc997e481562f45b9eb198abaf87374c2dd7a
Camille <camillemariuschatton@gmail.com>
2026-08-05 17:49:23 +0200

    added method getButtonState

--- esp/lib/gpioExpander/InputManager.cpp
+++ esp/lib/gpioExpander/InputManager.cpp
@@ -19,24 +19,46 @@ InputEvent InputManager::getEvent()
 
     // LOW = Taster gedrückt
     if (!(state & (1 << 3)))
-        return InputEvent::BUTTON_1;
+        return InputEvent::BT1;
 
     if (!(state & (1 << 4)))
-        return InputEvent::BUTTON_2;
+        return InputEvent::BT2;
 
     if (!(state & (1 << 5)))
-        return InputEvent::BUTTON_3;
+        return InputEvent::BT3;
 
     if (!(state & (1 << 10)))
-        return InputEvent::BUTTON_4;
+        return InputEvent::BT4;
 
     if (!(state & (1 << 11)))
-        return InputEvent::BUTTON_5;
+        return InputEvent::BT5;
 
     if (!(state & (1 << 12)))
-        return InputEvent::BUTTON_6;
+        return InputEvent::BT6;
 
     return InputEvent::NONE;
+}
+
+bool InputManager::getButtonState(InputEvent button)
+{
+    uint8_t pin = 0;
+
+    switch(button)
+    {
+        case InputEvent::BT1: pin = 3; break;
+        case InputEvent::BT2: pin = 4; break;
+        case InputEvent::BT3: pin = 5; break;
+        case InputEvent::BT4: pin = 10; break;
+        case InputEvent::BT5: pin = 11; break;
+        case InputEvent::BT6: pin = 12; break;
+
+        default:
+            return true;
+    }
+
+    uint16_t state = _expander.read16();
+
+    return (state & (1 << pin)) != 0;
 }
 
 void InputManager::setLed(InputEvent button, bool on)
@@ -45,12 +67,12 @@ void InputManager::setLed(InputEvent button, bool on)
 
     switch (button)
     {
-        case InputEvent::BUTTON_1: pin = 0;  break;
-        case InputEvent::BUTTON_2: pin = 1;  break;
-        case InputEvent::BUTTON_3: pin = 2;  break;
-        case InputEvent::BUTTON_4: pin = 13; break;
-        case InputEvent::BUTTON_5: pin = 14; break;
-        case InputEvent::BUTTON_6: pin = 15; break;
+        case InputEvent::BT1: pin = 2;  break;
+        case InputEvent::BT2: pin = 1;  break;
+        case InputEvent::BT3: pin = 0;  break;
+        case InputEvent::BT4: pin = 15; break;
+        case InputEvent::BT5: pin = 14; break;
+        case InputEvent::BT6: pin = 13; break;
         default: return;
     }
 
